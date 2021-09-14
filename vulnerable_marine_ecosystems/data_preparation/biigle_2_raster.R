a472368eeeeb97c5f2800e703fd9c91b1ded6006
charleygros <charley.gros@gmail.com>
2021-09-14 14:31:40 +1000

    biigle_2_raster

--- vulnerable_marine_ecosystems/data_preparation/biigle_2_raster.R
+++ vulnerable_marine_ecosystems/data_preparation/biigle_2_raster.R
@@ -1,55 +1,88 @@
 ################################################################################
 #                                 TODO
 ################################################################################
-# Take empty images into account
+# 
 
 ################################################################################
 #                                 INIT
 ################################################################################
 # Packages
-library(stringr)
+library(CCAMLRGIS)
+library(raster)
+library(SOmap)
+library(sp)
+
 
 # Set working directory
 setwd("C:/Users/cgros/code/IMAS/ARC_Benthic_Mapping/vulnerable_marine_ecosystems/data_preparation")
 
 # Path towards BIIGLE report
-path_biigle <- "C:/Users/cgros/code/biigle_scripts/20210908_biigle_vme.csv"
+path_biigle <- "C:/Users/cgros/code/IMAS/ARC_Benthic_Mapping/vulnerable_marine_ecosystems/20210909_biigle_vme_cover.csv"
 
+# Raster resolution
+resolution_raster <- 500
+
+# Output filename
+path_out <- "C:/Users/cgros/code/IMAS/ARC_Benthic_Mapping/vulnerable_marine_ecosystems/20210914_raster_cover.tif"
 
 ################################################################################
 #                                 XX
 ################################################################################
 # Read BIIGLE data
+cat("Reading data:", path_biigle, "...")
 df_biigle <- read.csv(path_biigle)
-df_biigle <- df_biigle[, c("label_hierarchy",
-                        "filename",
-                        "image_longitude",
-                        "image_latitude",
-                        "attributes",
-                        "points",
-                        "shape_name")]
-# Clean label names
-df_biigle$label <- gsub(" > ", "_", df_biigle$label_hierarchy)
-# Add survey column
-df_biigle$survey <- str_split_fixed(df_biigle$filename, "_", 2)[, 1]
-# Get image width, height, area
-df_biigle$width <- as.integer(str_split_fixed(str_split_fixed(df_biigle$attributes,
-                                                              '"width":', 2)[, 2],
-                                              ',', 2)[, 1])
-df_biigle$height <- as.integer(str_split_fixed(str_split_fixed(df_biigle$attributes,
-                                                              '"height":', 2)[, 2],
-                                              ',', 2)[, 1])
-df_biigle$area <- as.double(str_split_fixed(str_split_fixed(df_biigle$attributes,
-                                                               '"area":', 2)[, 2],
-                                               ',', 2)[, 1])
-# Fill missing values of width and height
+# Remove filename column
+cat("Cleaning data ...")
+df_biigle <- df_biigle[,!names(df_biigle) %in% c("filename")]
+# Convert survey ID to numeric
+df_biigle$survey <- as.numeric(df_biigle$survey)
+# Create new variable
+pts <- df_biigle
+# Projection
+coordinates(pts) <- c("longitude", "latitude")
+projection(pts) <- "+proj=longlat +datum=WGS84"
+pts <- SOproj(pts)
 
+cat("Cropping and resampling (",
+    resolution_raster, "x", resolution_raster, "m ) the reference raster...")
+# Crop reference raster according to pts extent
+raster_ref <- crop(SmallBathy, pts)
+# Set raster resolution
+if (resolution_raster < res(raster_ref)[1]) {
+  resample_fact <- res(raster_ref)[1] / resolution_raster
+  raster_ref <- disaggregate(raster_ref,
+                                  fact=resample_fact,
+                                  method='bilinear')
+} else if (resolution_raster > res(raster_ref)[1]) {
+  resample_fact <- resolution_raster / res(raster_ref)[1]
+  raster_ref <- aggregate(raster_ref,
+                               fact=resample_fact,
+                               method='bilinear')
+} else {
+  raster_ref <- raster_ref
+}
 
+cat("Using the projection system:", proj4string(raster_ref), "...")
+# Enforce same projection
+proj4string(pts) <- proj4string(raster_ref)
 
+# Rasterise data
+cat("Rasterizing data ...")
+raster_bio <- rasterize(pts,
+                        raster_ref,
+                        colnames(df_biigle)[!names(df_biigle) %in% c("longitude", "latitude", "survey")],
+                        fun=sum)
+raster_survey <- rasterize(pts,
+                           raster_ref,
+                           pts$survey,
+                           fun="first")
+# Stack rasters
+raster_tot <- stack(raster_bio, raster_survey)
+cat("Number of layers:", nlayers(raster_tot), "...")
 
-# Remove unnecessary columns
-df_biigle <- df_biigle[,!names(df_biigle) %in% c("label_hierarchy", "filename")]
-
-colnames(dataframe)[which(names(dataframe) == "columnName")] <- "newColumnName"
-
-
+# Save raster
+cat("Saving raster:", path_out, "...")
+writeRaster(raster_tot,
+            filename=path_out,
+            options="INTERLEAVE=BAND",
+            overwrite=TRUE)
