a11128d32e10fab6dcee30f852bebf3a9ec0687d
charleygros <charley.gros@gmail.com>
2021-09-30 16:39:19 +1000

    BAMMTools

--- vulnerable_marine_ecosystems/vme_index/utils.R
+++ vulnerable_marine_ecosystems/vme_index/utils.R
@@ -1,4 +1,6 @@
-library(readxl)    
+library(readxl)
+library(BAMMtools)
+
 read_excel_allsheets <- function(filename) {
   sheets <- readxl::excel_sheets(filename)
   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
