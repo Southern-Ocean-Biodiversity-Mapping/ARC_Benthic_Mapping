9a207b4aa7542836699ac2eef1d7aa6f78398bd5
charleygros <charley.gros@gmail.com>
2022-02-04 18:00:37 +1100

    combine_coralnet_biigle: consider image_size_sqpx

--- vulnerable_marine_ecosystems/data_preparation/combine_coralnet_biigle.py
+++ vulnerable_marine_ecosystems/data_preparation/combine_coralnet_biigle.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import argparse
 import pandas as pd
 import pyreadr
@@ -8,7 +7,7 @@ import curate_biigle_report
 
 
 # Example:
-#   python data_preparation\combine_coralnet_biigle.py -b biodata_step2.csv -c biodata_step3.csv -m C:\Users\cgros\code\IMAS\ARC_Data\annotation\Circumpolar_Annotation_Data.Rdata -a coverage_biigle839 -o biodata_step4.csv
+#   python data_preparation\combine_coralnet_biigle.py -b biodata_step2.csv -c biodata_step3.csv -m C:\Users\cgros\code\IMAS\ARC_Data\annotation\Circumpolar_Annotation_Data.Rdata -a coverage_biigle839 -p px_size.csv -o biodata_step4.csv
 
 
 LST_BIIGLE839_FULL = ["PS81_shallow", "PS61", "PS14", "PS06", "JR17001", "JR262", "AA2011", "JR15005", "PS96", "JR17003"]
@@ -28,6 +27,8 @@ def get_parser():
                                 help='Metadata Rdata filename.')
     mandatory_args.add_argument('-a', '--afolder', required=True, type=str,
                                 help='Folder containing the area covered by BIIGLE 839 annotations.')
+    mandatory_args.add_argument('-p', '--pfname', required=True, type=str,
+                                help='File containing the area, in sqpix, of each image.')
     mandatory_args.add_argument('-o', '--ofname', required=True, type=str,
                                 help='Output csv filename.')
 
@@ -39,14 +40,17 @@ def get_parser():
     return parser
 
 
-def combine_coralnet_biigle(fname_biigle, fname_coralnet, fname_metadata, folder_area_notfull, fname_o):
+def combine_coralnet_biigle(fname_biigle, fname_coralnet, fname_metadata, folder_area_notfull, fname_sqpx, fname_o):
     # Read data
     df_b = pd.read_csv(fname_biigle)
     df_c = pd.read_csv(fname_coralnet)
     df_m = pyreadr.read_r(fname_metadata)["image_metadata"].reset_index()
+    df_p = pd.read_csv(fname_sqpx)[["filename", "image_size_sqpx"]]
+
+    print("\nWARNING: If add AREA: carefull of summing differently between CoralNet BIIGLE839 BIIGLE254.\n")
 
     # Cleanup
-    df_m.drop(columns=["rownames", "proj_coord_x", "proj_coord_y", "cellID"], inplace=True)
+    df_m.drop(columns=["rownames", "proj_coord_x", "proj_coord_y", 'area_source', 'image_quality_score', 'year', 'gear', 'area'], inplace=True)
     df_m.rename(columns={"Filename.standardised": "filename",
                          "lon": "longitude",
                          "lat": "latitude",
@@ -56,10 +60,10 @@ def combine_coralnet_biigle(fname_biigle, fname_coralnet, fname_metadata, folder
     df_m.loc[:, "biigle254"] = [1 if i == "yes" else 0 for i in df_m.biigle254.tolist()]
     df_m.loc[:, "biigle839"] = [0 for i in df_m.coralnet.tolist()]
     df_m.drop(index=df_m[df_m.coralnet == 0].index, inplace=True)
+    df_m["cellID"] = df_m["cellID"].astype(int)
 
     # Fill BIIGLE839
     # Get infos
-    df_m["survey"] = df_m["filename"].str.split('_').str[0]
     df_m["transect"] = df_m["filename"].str.split('_').str[1]
     df_m["imageID"] = df_m["filename"].str.split('_').str[2]
     # Fill 1s the surveys that have been fully annotated
@@ -109,17 +113,18 @@ def combine_coralnet_biigle(fname_biigle, fname_coralnet, fname_metadata, folder
 
     df_m.drop(columns=["transect", "imageID"], inplace=True)
 
-    print("\nRemoving {} images because area is unknown ...".format(len(df_m[df_m.area.isnull()])))
-    filename_to_exclude = df_m[df_m.area.isnull()]["filename"].tolist()
-    df_m = df_m[~df_m.filename.isin(filename_to_exclude)]
-    df_b = df_b[~df_b.filename.isin(filename_to_exclude)]
-    df_c = df_c[~df_c.filename.isin(filename_to_exclude)]
+    #print("\nRemoving {} images because area is unknown ...".format(len(df_m[df_m.area.isnull()])))
+    #filename_to_exclude = df_m[df_m.area.isnull()]["filename"].tolist()
+    #df_m = df_m[~df_m.filename.isin(filename_to_exclude)]
+    #df_b = df_b[~df_b.filename.isin(filename_to_exclude)]
+    #df_c = df_c[~df_c.filename.isin(filename_to_exclude)]
 
-    print("\nRemoving {} images because no annotation on CoralNet ...".format(len(df_c[df_c.n_annotation == 0])))
     filename_to_exclude = df_c[df_c.n_annotation == 0]["filename"].tolist()
-    df_m = df_m[~df_m.filename.isin(filename_to_exclude)]
-    df_b = df_b[~df_b.filename.isin(filename_to_exclude)]
-    df_c = df_c[~df_c.filename.isin(filename_to_exclude)]
+    if len(filename_to_exclude):
+        print("\nRemoving {} images because no annotation on CoralNet ...".format(len(df_c[df_c.n_annotation == 0])))
+        df_m = df_m[~df_m.filename.isin(filename_to_exclude)]
+        df_b = df_b[~df_b.filename.isin(filename_to_exclude)]
+        df_c = df_c[~df_c.filename.isin(filename_to_exclude)]
 
     print("\nNumber of images annotated with:")
     print("\tCoralNet: {} ...".format(len(df_m[df_m["coralnet"] == 1].index)))
@@ -135,30 +140,51 @@ def combine_coralnet_biigle(fname_biigle, fname_coralnet, fname_metadata, folder
         dct_no_ann[taxon_biigle] = [0 for _ in list_no_ann_biigle]
     df_b = pd.concat([df_b, pd.DataFrame.from_dict(dct_no_ann)])
 
-    print("\nTODO: Check missing Area PIX missing data.")
+    df_b = pd.merge(df_p, df_b, on="filename", how="right")
 
-    # Get source for each taxon
-    dct_taxon_source = {"morpho_taxon": [], "source": []}
-    for taxon_coralnet in [c for c in df_c.keys() if c not in ["filename", "n_annotation"]]:
-        dct_taxon_source["morpho_taxon"].append(taxon_coralnet)
-        dct_taxon_source["source"].append("coralnet")
-    for taxon_biigle in [c for c in df_b.keys() if c not in ['filename']]:
-        dct_taxon_source["morpho_taxon"].append(taxon_biigle)
-        if taxon_biigle in curate_biigle_report.DCT_BIIGLE254.values():
-            dct_taxon_source["source"].append("biigle254")
-        else:
-            dct_taxon_source["source"].append("biigle839")
-    df_taxon_source = pd.DataFrame.from_dict(dct_taxon_source)
+    df_c = pd.merge(df_m[['cellID', 'filename']], df_c, on="filename", how="right")
+    df_b = pd.merge(df_m[['cellID', 'filename']], df_b, on="filename", how="right")
+
+    lst_taxa_coralnet = [t for t in df_c.keys() if t not in ["cellID", "filename", "n_annotation"]]
+    lst_taxa_254 = list(set(curate_biigle_report.DCT_BIIGLE254.values()))
+    lst_taxa_839 = [t for t in df_b.keys() if t not in ["cellID", "filename", "image_size_sqpx"] + list(curate_biigle_report.DCT_BIIGLE254.values())]
+
+    df_b_254 = df_b[["cellID", "filename", "image_size_sqpx"] + lst_taxa_254]
+    df_b_839 = df_b[["cellID", "filename", "image_size_sqpx"] + lst_taxa_839]
+
+    df_c = df_c.drop(index=df_c[~df_c.filename.isin(df_m[df_m.coralnet == 1].filename)].index)
+    df_b_254 = df_b_254.drop(index=df_b_254[~df_b_254.filename.isin(df_m[df_m.biigle254 == 1].filename)].index)
+    df_b_839 = df_b_839.drop(index=df_b_839[~df_b_839.filename.isin(df_m[df_m.biigle839 == 1].filename)].index)
+
+    df_c.drop(columns=["filename"], inplace=True)
+    df_b_254.drop(columns=["filename"], inplace=True)
+    df_b_839.drop(columns=["filename"], inplace=True)
+
+    df_c_pc = df_c.groupby(['cellID'], as_index=False).sum()
+    df_b_254_pc = df_b_254.groupby(['cellID'], as_index = False).sum()
+    df_b_839_pc = df_b_839.groupby(['cellID'], as_index = False).sum()
+
+    df_c_pc[lst_taxa_coralnet] = df_c_pc[lst_taxa_coralnet].div(df_c_pc['n_annotation'].values, axis=0) * 100
+    df_b_254_pc[lst_taxa_254] = df_b_254_pc[lst_taxa_254].div(df_b_254_pc['image_size_sqpx'].values, axis=0) * 100
+    df_b_839_pc[lst_taxa_839] = df_b_839_pc[lst_taxa_839].div(df_b_839_pc['image_size_sqpx'].values, axis=0) * 100
+
+    df_c_pc.drop(columns=['n_annotation'], inplace=True)
+    df_b_254_pc.drop(columns=['image_size_sqpx'], inplace=True)
+    df_b_839_pc.drop(columns=['image_size_sqpx'], inplace=True)
+
+    print("\nRemoving the cells where all CoralNet, BIIGLE254 and BIIGLE839 are present ...")
+    lst_cellID_intersection = [c for c in df_c_pc.cellID if c in df_b_254_pc.cellID.to_list() and c in df_b_839_pc.cellID.to_list()]
+    df_c_pc.drop(index=df_c_pc[~df_c_pc.cellID.isin(lst_cellID_intersection)].index, inplace=True)
+    df_b_254_pc.drop(index=df_b_254_pc[~df_b_254_pc.cellID.isin(lst_cellID_intersection)].index, inplace=True)
+    df_b_839_pc.drop(index=df_b_839_pc[~df_b_839_pc.cellID.isin(lst_cellID_intersection)].index, inplace=True)
 
     print("\nMerging datasets ...")
-    df_merged = pd.merge(df_m, df_b, on="filename", how="left")
-    df_merged = pd.merge(df_merged, df_c, on="filename", how="left")
+    df_out = pd.merge(df_c_pc, df_b_254_pc, on="cellID", how="left")
+    df_out = pd.merge(df_out, df_b_839_pc, on="cellID", how="left")
+    df_out["cellID"] = df_out["cellID"].astype(int)
 
     print("\nSaving data: {} ...".format(fname_o))
-    df_merged.to_csv(fname_o, index=False)
-    fname_o_src = fname_o.split(".csv")[0] + "_source.csv"
-    print("\nSaving data: {} ...".format(fname_o_src))
-    df_taxon_source.to_csv(fname_o_src, index=False)
+    df_out.to_csv(fname_o, index=False)
 
 
 def main():
@@ -170,6 +196,7 @@ def main():
                             fname_coralnet=args.cfname,
                             fname_metadata=args.mfname,
                             folder_area_notfull=args.afolder,
+                            fname_sqpx=args.pfname,
                             fname_o=args.ofname)
 
 
