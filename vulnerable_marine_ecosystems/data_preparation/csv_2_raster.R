2568e8244ae5620dae79f2a22ddb4b97b889d884
charleygros <charley.gros@gmail.com>
2022-02-22 11:47:23 +1100

    csv_2_raster: mapview

--- vulnerable_marine_ecosystems/data_preparation/csv_2_raster.R
+++ vulnerable_marine_ecosystems/data_preparation/csv_2_raster.R
@@ -8,25 +8,27 @@
 #                                 INIT
 ################################################################################
 # Packages
-library(CCAMLRGIS)
-library(raster)
-library(SOmap)
+#library(CCAMLRGIS)
+#library(SOmap)
 library(sp)
-library(proj4)
+#library(proj4)
 library(dplyr)
+
+library(mapview)
+library(raster)
 
 # Set working directory
 setwd("C:/Users/cgros/code/IMAS/ARC_Benthic_Mapping/vulnerable_marine_ecosystems")
 
 # Path to BIO data
-path_bio_data <- "C:/Users/cgros/code/IMAS/ARC_Benthic_Mapping/vulnerable_marine_ecosystems/biodata_step4.csv"
+path_bio_data <- "C:/Users/cgros/code/IMAS/ARC_Benthic_Mapping/vulnerable_marine_ecosystems/20220218161751_002/df_vme_idx.csv"
 
 # Path to REF Raster
 path_ref_raster <- "C:/Users/cgros/data/SO_env_layers/derived/Circumpolar_EnvData_500m_shelf_bathy_gebco_depth"
 
 # Path to Coastline
-path_coastline <- "C:/Users/cgros/code/IMAS/ARC_Data/prep_environment/Circumpolar_Coastline.Rdata"
-load(path_coastline)
+#path_coastline <- "C:/Users/cgros/code/IMAS/ARC_Data/prep_environment/Circumpolar_Coastline.Rdata"
+#load(path_coastline)
 
 ################################################################################
 #                                 READ INPUT DATA
@@ -34,18 +36,52 @@ load(path_coastline)
 
 # Read Bio data
 cat("\nReading data:", path_bio_data, "...\n")
-df_abd <- read.csv(path_bio_data)
-
-# Create PA data
-lst_taxa <- colnames(df_abd)
-lst_taxa <- lst_taxa[!lst_taxa == "cellID"]
-df_pa <- data.frame(df_abd)
-for (t in lst_taxa) {
-  df_pa[ , t] <- as.integer(as.logical(df_pa[ , t]))
-}
+df_idx <- read.csv(path_bio_data)
 
 # Load REF raster
 r_ref <- raster(path_ref_raster)
+
+df_xy <- xyFromCell(r_ref, df_idx$cellID)
+df_idx$x <- df_xy[, 1]
+df_idx$y <- df_xy[, 2]
+
+col_names <- colnames(df_idx)
+col_not_coords <- col_names[col_names != "x" & col_names != "y"]
+
+area_1 = list("xmin"=-3e6, "xmax"=-2e6,
+              "ymin"=0, "ymax"=3e6)
+area_2 = list("xmin"=-1e6, "xmax"=1e6,
+              "ymin"=-2.5e6, "ymax"=-1e6)
+area_3 = list("xmin"=1e6, "xmax"=3e6,
+              "ymin"=-3e6, "ymax"=-1e6)
+area_4 = list("xmin"=-1.5e6, "xmax"=1e6,
+              "ymin"=0.5e6, "ymax"=2.5e6)
+area = area_1
+df_idx_area = df_idx[(df_idx$x > area$xmin) &
+                       (df_idx$x < area$xmax) &
+                       (df_idx$y > area$ymin) &
+                       (df_idx$y < area$ymax), ]
+min(df_idx_area$x)
+max(df_idx_area$x)
+min(df_idx_area$y)
+max(df_idx_area$y)
+# TODO: work on how to get all raster layers
+r_idx_area <- rasterFromXYZ(df_idx_area[c("x", "y", "VME.index")], res=res(r_ref), crs=crs(r_ref))
+
+# TODO: work on how to 
+mapview(r_idx_area, maxpixels=9225970, na.color="transparent")
+
+
+
+
+
+
+
+
+
+
+
+
 
 ################################################################################
 #                                RASTERIZE
@@ -53,8 +89,8 @@ r_ref <- raster(path_ref_raster)
 
 r_bio = setValues(r_ref, NA)
 
-r_bio[df_abd$cellID] = df_abd[[lst_taxa[1]]]
-names(r_bio) <- lst_taxa[1]
+r_bio[df_idx$cellID] = df_idx$VME.index
+names(r_bio) <- "VME index"
 
 for (idx_taxa in 2:4) {
   print(lst_taxa[idx_taxa])
@@ -64,6 +100,17 @@ for (idx_taxa in 2:4) {
   r_bio <- stack(r_bio, r_tmp)
   remove(r_tmp)
 }
+
+
+
+
+
+
+
+
+
+
+
 
 buffer_size <- 10000
 rnd_cell <- sample(1:nrow(df_abd), 1)
